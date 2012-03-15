57163eecd9c387acab890b52573ac4ee01958290
musich <>
2012-03-15 14:27:54 +0000

    script to produce finalplots

--- BTagStudies/scripts/ExecutePlotting.C
+++ BTagStudies/scripts/ExecutePlotting.C
@@ -1,12 +1,38 @@
-{
+void ExecutePlotting(TString foldername){
   
-  gSystem->mkdir("figures_jetbyjet");
-  gROOT->LoadMacro("./JetInfo.cxx++g");
+  /* available files
+     
+     JetByJetComparisonTree_DeltaZ500noAPEVsIdealnoAPE.root
+     JetByJetComparisonTree_DeltaZ500VsDeltaZ500noAPE.root
+     JetByJetComparisonTree_DeltaZ500VsIdeal.root
+     JetByJetComparisonTree_IdealVsIdealnoAPE.root
+     JetByJetComparisonTree_Startup2011VsIdeal.root
+     JetByJetComparisonTree_DeltaZ40VsDeltaZ40noAPE.root                                          
+     JetByJetComparisonTree_DeltaZ40VsIdeal.root     
+  */
+
+  std::string foldername_s = foldername;
+  
+  gSystem->mkdir(foldername);
+  gROOT->LoadMacro("./JetInfo.cxx+");
+
+  gROOT->LoadMacro("./tdrStyle.C+");
+  setTDRStyle("gray");
+  //setTDRStyle("logredblue");
+  gROOT->LoadMacro("./JetByJetComparisonHistos.cxx++g");
   gROOT->LoadMacro("./JetByJetComparison.C++g");
-  JetByJetComparison t("JetByJetComparison_origVsnewgeom.root");
+  JetByJetComparison t("../data/JetByJetComparisonTree_"+foldername+".root");
   t.Loop(); 
   
-  TString processline = Form(".! mv *.png figures_jetbyjet") ;
+  gROOT->LoadMacro("./diow.C+");
+  diow(".","index.html");
+
+  TString processline = Form(".! mv *.png index.html %s",foldername_s) ;
   gROOT->ProcessLine(processline.Data());
   gSystem->Sleep(100);
+  processline.Clear();
+
+  processline = Form(".! cp diow.C %s",foldername_s) ;
+  gROOT->ProcessLine(processline.Data());
+ 
 }
